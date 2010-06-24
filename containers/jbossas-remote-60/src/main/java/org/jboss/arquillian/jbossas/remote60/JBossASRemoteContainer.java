72746ce963c9e45aa1c045d5b74f0441e00cf6f3
Thomas Diesler <thomas.diesler@jboss.com>
2010-06-24 08:39:47 +0000

    OSGi: Add a simple test case

--- containers/jbossas-remote-60/src/main/java/org/jboss/arquillian/jbossas/remote60/JBossASRemoteContainer.java
+++ containers/jbossas-remote-60/src/main/java/org/jboss/arquillian/jbossas/remote60/JBossASRemoteContainer.java
@@ -162,7 +162,7 @@ public class JBossASRemoteContainer implements DeployableContainer
       }
       if (failure != null)
       {
-         throw new DeploymentException("Failed to deply " + deploymentName, failure);
+         throw new DeploymentException("Failed to deploy " + deploymentName, failure);
       }
       try 
       {
