d9dad1c199ccfe88d39ef5f6a4310f0c607689a5
Dan Allen <dan.j.allen@gmail.com>
2010-07-04 06:35:45 +0000

    ARQ-145 Jetty Embedded 7 container

---
+++ containers/jetty-embedded-7/src/main/java/org/jboss/arquillian/container/jetty/embedded_7/JettyEmbeddedContainer.java
@@ -0,0 +1,172 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2010, Red Hat Middleware LLC, and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.arquillian.container.jetty.embedded_7;
+
+import java.net.URL;
+import java.util.logging.Logger;
+import org.eclipse.jetty.server.Connector;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.HandlerCollection;
+import org.eclipse.jetty.server.nio.SelectChannelConnector;
+import org.eclipse.jetty.webapp.WebAppContext;
+
+import org.jboss.arquillian.protocol.servlet.ServletMethodExecutor;
+import org.jboss.arquillian.spi.Configuration;
+import org.jboss.arquillian.spi.ContainerMethodExecutor;
+import org.jboss.arquillian.spi.Context;
+import org.jboss.arquillian.spi.DeployableContainer;
+import org.jboss.arquillian.spi.DeploymentException;
+import org.jboss.arquillian.spi.LifecycleException;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.jetty_7.api.ShrinkWrapWebAppContext;
+
+/**
+ * <p>Jetty Embedded 7.x and 8.x container for the Arquillian project.</p>
+ *
+ * <p>This container only supports a WebArchive deployment. The context path of the
+ * deployed application is always set to "/test", which is expected by the Arquillian
+ * servlet protocol.</p>
+ *
+ * <p>Another known issue is that the container configuration process logs an exception when
+ * running in-container. However, the container is still configured properly during setup.</p>
+ *
+ * @author Dan Allen
+ * @version $Revision: $
+ */
+public class JettyEmbeddedContainer implements DeployableContainer
+{
+   public static final String HTTP_PROTOCOL = "http";
+
+   public static final String WEB_APP_CONTEXT_PATH = "/test";
+
+   public static final String[] JETTY_PLUS_CONFIGURATION_CLASSES =
+   {
+       "org.eclipse.jetty.webapp.WebInfConfiguration",
+       "org.eclipse.jetty.webapp.WebXmlConfiguration",
+       "org.eclipse.jetty.webapp.MetaInfConfiguration",
+       "org.eclipse.jetty.webapp.FragmentConfiguration",
+       "org.eclipse.jetty.plus.webapp.EnvConfiguration",
+       "org.eclipse.jetty.plus.webapp.Configuration",
+       "org.eclipse.jetty.webapp.JettyWebXmlConfiguration"
+   };
+
+   private static final Logger log = Logger.getLogger(JettyEmbeddedContainer.class.getName());
+
+   private Server server;
+
+   private JettyEmbeddedConfiguration containerConfig;
+   
+   public JettyEmbeddedContainer()
+   {
+   }
+   
+   public void setup(Context context, Configuration arquillianConfig)
+   {
+      containerConfig = arquillianConfig.getContainerConfig(JettyEmbeddedConfiguration.class);
+   }
+   
+   public void start(Context context) throws LifecycleException
+   {
+      try 
+      {
+         server = new Server();
+         Connector connector = new SelectChannelConnector();
+         connector.setHost(containerConfig.getBindAddress());
+         connector.setPort(containerConfig.getBindHttpPort());
+         server.setConnectors(new Connector[] { connector });
+         server.setHandler(new HandlerCollection(true));
+         log.info("Starting Jetty Embedded Server " + Server.getVersion() + " [id:" + server.hashCode() + "]");
+         server.start();
+      } 
+      catch (Exception e) 
+      {
+         throw new LifecycleException("Could not start container", e);
+      }
+   }
+
+   public void stop(Context context) throws LifecycleException
+   {
+      try
+      {
+         log.info("Stopping Jetty Embedded Server [id:" + server.hashCode() + "]");
+         server.stop();
+      } 
+      catch (Exception e) 
+      {
+         throw new LifecycleException("Could not stop container", e);
+      }
+   }
+
+   public ContainerMethodExecutor deploy(Context context, Archive<?> archive) throws DeploymentException
+   {
+      try 
+      {
+         WebAppContext wctx = archive.as(ShrinkWrapWebAppContext.class);
+         // Jetty plus is required to support in-container invocation and enrichment
+         if (containerConfig.isJettyPlus())
+         {
+            wctx.setConfigurationClasses(JETTY_PLUS_CONFIGURATION_CLASSES);
+         }
+         // FIXME shrinkwrap jetty adapter does not remove file extension from name when setting context path
+         wctx.setContextPath(WEB_APP_CONTEXT_PATH);
+         // possible configuration parameters
+         wctx.setExtractWAR(true);
+         wctx.setLogUrlOnStart(true);
+         ((HandlerCollection) server.getHandler()).addHandler(wctx);
+         wctx.start();
+         context.add(WebAppContext.class, wctx);
+      } 
+      catch (Exception e) 
+      {
+         throw new DeploymentException("Could not deploy " + archive.getName(), e);
+      }
+
+      try 
+      {
+         return new ServletMethodExecutor(
+               new URL(
+                     HTTP_PROTOCOL,
+                     containerConfig.getBindAddress(),
+                     containerConfig.getBindHttpPort(),
+                     "/")
+               );
+      } 
+      catch (Exception e) 
+      {
+         throw new RuntimeException("Could not create ContainerMethodExecutor", e);
+      }
+   }
+
+   public void undeploy(Context context, Archive<?> archive) throws DeploymentException
+   {
+      WebAppContext wctx = context.get(WebAppContext.class);
+      if (wctx != null)
+      {
+         try
+         {
+            wctx.stop();
+         }
+         catch (Exception e)
+         {
+            e.printStackTrace();
+            log.severe("Could not stop context " + wctx.getContextPath() + ": " + e.getMessage());
+         }
+         ((HandlerCollection) server.getHandler()).removeHandler(wctx);
+      }
+   }
+
+}
---
+++ containers/jetty-embedded-7/src/test/java/org/jboss/arquillian/container/jetty/embedded_7/JettyEmbeddedInContainerTestCase.java
@@ -0,0 +1,107 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2010, Red Hat Middleware LLC, and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.arquillian.container.jetty.embedded_7;
+
+import java.sql.Connection;
+import java.util.logging.Logger;
+import javax.annotation.Resource;
+import javax.inject.Inject;
+import javax.sql.DataSource;
+
+import org.jboss.arquillian.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.jboss.shrinkwrap.impl.base.asset.ByteArrayAsset;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * In-container test case for the Jetty Embedded 7 container
+ *
+ * @author Dan Allen
+ * @version $Revision: $
+ */
+@RunWith(Arquillian.class)
+public class JettyEmbeddedInContainerTestCase
+{
+   private static final Logger log = Logger.getLogger(JettyEmbeddedInContainerTestCase.class.getName());
+   
+   /**
+    * Deployment for the test
+    */
+   @Deployment
+   public static WebArchive getTestArchive()
+   {
+      final WebArchive war = ShrinkWrap.create("in-container-test.war", WebArchive.class)
+         .addClass(TestBean.class)
+         // adding the configuration class silences the logged exception when building the configuration on the server-side, but shouldn't be necessary
+         //.addClass(JettyEmbeddedConfiguration.class)
+         .addLibrary(MavenArtifactResolver.resolve("org.jboss.weld.servlet:weld-servlet:1.0.1-Final"))
+         .addWebResource("jetty-env.xml")
+         .addWebResource(new ByteArrayAsset(new byte[0]), "beans.xml")
+         .setWebXML("in-container-web.xml");
+      log.info(war.toString(true));
+      return war;
+   }
+
+   // defined in jetty-env.xml, scoped to global
+   @Resource(mappedName = "version") Integer version;
+
+   // defined in web.xml, scoped to webapp (relative to java:comp/env)
+   @Resource(name = "name") String name;
+
+   // defined in jetty-env.xml, scoped to webapp (relative to java:comp/env)
+   @Resource(name = "type") String containerType;
+
+   @Resource(name = "jdbc/test") DataSource ds;
+
+   @Inject TestBean testBean;
+
+   @Test
+   public void shouldBeAbleToInjectMembersIntoTestClass() throws Exception
+   {
+      Assert.assertNotNull(version);
+      Assert.assertEquals(new Integer(6), version);
+      Assert.assertNotNull(name);
+      Assert.assertEquals("Jetty", name);
+      Assert.assertNotNull(containerType);
+      Assert.assertEquals("Embedded", containerType);
+      Assert.assertNotNull(ds);
+      Connection c = null;
+      try
+      {
+         c = ds.getConnection();
+         Assert.assertEquals("H2", c.getMetaData().getDatabaseProductName());
+         c.close();
+      }
+      catch (Exception e)
+      {
+         Assert.fail(e.getMessage());
+      }
+      finally
+      {
+         if (c != null && !c.isClosed())
+         {
+            c.close();
+         }
+      }
+      Assert.assertNotNull(testBean);
+      Assert.assertEquals("Jetty", testBean.getName());
+   }
+}
