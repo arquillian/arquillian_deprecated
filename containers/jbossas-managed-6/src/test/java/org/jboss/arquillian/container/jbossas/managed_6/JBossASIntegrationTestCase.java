c5c25d1953141f6311823d1e5dee17bef48964f1
Aslak Knutsen <aslak@redhat.com>
2011-01-17 12:44:35 +0100

    ARQ-196 Update arquillian.xml

--- containers/jbossas-managed-6/src/test/java/org/jboss/arquillian/container/jbossas/managed_6/JBossASIntegrationTestCase.java
+++ containers/jbossas-managed-6/src/test/java/org/jboss/arquillian/container/jbossas/managed_6/JBossASIntegrationTestCase.java
@@ -18,13 +18,12 @@ package org.jboss.arquillian.container.jbossas.managed_6;
 
 import javax.ejb.EJB;
 
-import junit.framework.Assert;
-
 import org.jboss.arquillian.api.Deployment;
 import org.jboss.arquillian.container.jbossas.managed_6.ejb.MyEjb;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
