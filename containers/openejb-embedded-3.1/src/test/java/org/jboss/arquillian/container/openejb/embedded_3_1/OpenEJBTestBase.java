4f83a3b9f735531bf5982dfcc34151b8f0961bd1
drallen <david.allen@acm.org>
2011-03-13 13:20:18 +0100

    ARQ-372 Fix to inject @EJB fields in super test classes

---
+++ containers/openejb-embedded-3.1/src/test/java/org/jboss/arquillian/container/openejb/embedded_3_1/OpenEJBTestBase.java
@@ -0,0 +1,19 @@
+package org.jboss.arquillian.container.openejb.embedded_3_1;
+
+import javax.annotation.Resource;
+import javax.ejb.EJB;
+import javax.sql.DataSource;
+
+import org.jboss.arquillian.container.openejb.embedded_3_1.ejb.SimpleLocalBusiness;
+import org.jboss.arquillian.junit.Arquillian;
+import org.junit.runner.RunWith;
+
+@RunWith(Arquillian.class)
+public class OpenEJBTestBase
+{
+   @EJB
+   protected SimpleLocalBusiness simpleBean;
+
+   @Resource(name="testDatabase")
+   protected DataSource testDatabase;
+}
